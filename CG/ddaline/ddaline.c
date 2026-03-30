d932f812a9ef5ca4c3a0c1ad9af0a3f2b05070e7
Theerthananda <theerthanandagowda@gmail.com>
2026-03-30 21:10:18 +0530

    added dda and Bresenham line drawing alogorithm

---
+++ CG/ddaline/ddaline.c
@@ -0,0 +1,50 @@
+/*
+ * @author Theerthananda
+ */
+
+ //DDA Line Drawing Algorithm
+#include <graphics.h>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int gd = DETECT, gm;
+    int x1, y1, x2, y2;
+    float dx, dy, steps, xinc, yinc, x, y;
+
+    initgraph(&gd, &gm, "");
+
+    printf("Enter starting point (x1, y1): ");
+    scanf("%d %d", &x1, &y1);
+
+    printf("Enter ending point (x2, y2): ");
+    scanf("%d %d", &x2, &y2);
+
+    dx = x2 - x1;
+    dy = y2 - y1;
+
+    if (abs(dx) > abs(dy))
+        steps = abs(dx);
+    else
+        steps = abs(dy);
+
+    xinc = dx / steps;
+    yinc = dy / steps;
+
+    x = x1;
+    y = y1;
+
+    for (int i = 0; i <= steps; i++)
+    {
+    	delay(1000);
+        putpixel(round(x), round(y), WHITE);
+        x = x + xinc;
+        y = y + yinc;
+    }
+
+    getch();
+    closegraph();
+
+    return 0;
+}
