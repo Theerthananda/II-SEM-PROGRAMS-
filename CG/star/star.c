d0881a6ab204b9ef3633dc34d3a6a4db051b70f4
Theerthananda <theerthanandagowda@gmail.com>
2026-03-31 22:33:05 +0530

    .

---
+++ CG/star/star.c
@@ -0,0 +1,28 @@
+#include <graphics.h>
+#include <conio.h>
+
+int main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+
+
+    int star[] = {
+        300, 100,
+        340, 200,
+        450, 200,
+        360, 260,
+        400, 360,
+        300, 300,
+        200, 360,
+        240, 260,
+        150, 200,
+        260, 200,
+        300, 100
+    };
+
+    drawpoly(11, star);
+
+    getch();
+    closegraph();
+    return 0;
+}
